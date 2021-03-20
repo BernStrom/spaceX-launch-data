aefd869f8a69340e2ba5b36730343b9cc2e39dd9
Bern <bernnystrom@gmail.com>
2021-03-20 20:59:07 +0800

    Change api to version 3 and add launch rocket name to flight data response

--- mod.ts
+++ mod.ts
@@ -3,18 +3,16 @@ import * as log from 'https://deno.land/std@0.90.0/log/mod.ts';
 interface Launch {
   flightNumber: number;
   mission: string;
+  rocket: string;
 }
 
 const launches = new Map<number, Launch>();
 
 const downloadLaunchData = async () => {
   log.info('Downloading launch data...');
-  const response = await fetch(
-    'https://api.spacexdata.com/v4/launches',
-    {
-      method: 'GET',
-    }
-  );
+  const response = await fetch('https://api.spacexdata.com/v3/launches', {
+    method: 'GET',
+  });
 
   if (!response.ok) {
     log.warning('Problem downloading launch data');
@@ -25,7 +23,8 @@ const downloadLaunchData = async () => {
   for (const launch of launchData) {
     const flightData = {
       flightNumber: launch['flight_number'],
-      mission: launch['name'],
+      mission: launch['mission_name'],
+      rocket: launch['rocket']['rocket_name'],
     };
 
     launches.set(flightData.flightNumber, flightData);
